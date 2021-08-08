4f3f6ab1f5b1a5300c326f6ba5b9236cd2bdb41c
sambe <stb41@cam.ac.uk>
2021-08-08 13:09:55 +0100

    simulate an entire match. find winner and print statistics

--- MatchSim.py
+++ MatchSim.py
@@ -15,52 +15,114 @@ class inningsState:
     def __init__(self):
         self.runs = 0
         self.extras = 0
+        self.total = 0
         self.wickets = 0
         self.totalBalls = 0
         self.legalBalls = 0
+        # extra stats
+        self.fallOfWickets = {}
+        self.fours = 0
+        self.sixes = 0
+        self.history = []
+        self.maidens = 0
 
     def updateState(self, ball):
         self.runs = self.runs + ball.runs
         self.extras = self.extras + ball.extras
+        self.total = self.runs + self.extras
         self.wickets = self.wickets + ball.wicket
         self.totalBalls = self.totalBalls + 1
         self.legalBalls = self.legalBalls + ball.overIncrementsBy
+        self.history.append(ball)
+        self.updateStatistics(ball)
+        if self.legalBalls % 6 == 0: # at end of over (and not at first ball of )
+            if len(self.history) == 6:
+                sixDots = True
+                for b in self.history:
+                    if b.runs >0 or b.overIncrementsBy == 0: # not a dot if runs scored or a wide or a no ball
+                        sixDots = False
+                if sixDots:
+                    self.maidens = self.maidens + 1
+            if ball.overIncrementsBy == 1:  # don't empty if a bowler extra (at start of over)
+                self.history = []   # empty history for next over
+
+
+    def updateStatistics(self, ball):
+        if ball.wicket:
+            self.fallOfWickets[self.wickets] = self.total   # make sure to be aware if running before or after updatestate
+        if ball.runs == 4:
+            self.fours = self.fours + 1
+        if ball.runs == 6:
+            self.sixes = self.sixes + 1
+
+    def printStatistics(self):
+        print('Fours:'.rjust(10), str(self.fours).ljust(5))
+        print('Sixes:'.rjust(10), str(self.sixes).ljust(5))
+        print('Maidens:'.rjust(10), str(self.maidens).ljust(5))
+
+        print('Wicket:'.rjust(10), ' '.join(str(key).ljust(5) for key in self.fallOfWickets))
+        print('Runs:'.rjust(10), ' '.join(str(self.fallOfWickets[key]).ljust(5) for key in self.fallOfWickets))
 
     def printState(self):
         overs = self.legalBalls // 6
         balls = self.legalBalls % 6
-        print(str(self.runs) + '-' + str(self.wickets) + '  ' + str(overs) + '.' + str(balls) + ' Overs')
+        print(str(self.total) + '-' + str(self.wickets) + '  ' + str(overs) + '.' + str(balls) + ' Overs')
 
 class MatchSim:
 
     def __init__(self, fileDirectory):
         self.fileDirectory = fileDirectory
-        inState = inningsState()
         self.models = self.createDictOfModels()
         self.outcomeMarkov = self.models['outcomes.txt']
+        self.outcomeDict = self.initialiseOutcomes()
         print('madeMatchSim')
+
+    def simulateMatch(self):
+        inningsA = self.simulateOneInnings()
+        inningsB = self.simulateOneInnings(target=inningsA.total)
+        inningsA.printStatistics();
+        print()
+        inningsB.printStatistics()
+        self.compareInnings(inningsA, inningsB)
+
+    def simulateOneInnings(self, target=10000, showOutput=True, scoreEveryBall=False, commentary=False):
+        inState = inningsState()
         prefix = ['START0', 'START1']
-        self.batterRuns = 0
-        self.extraRuns = 0
-        self.validBalls = 0
-        self.totalBalls = 0
-        self.wickets = 0
-        outcomeDict = self.initialiseOutcomes()
-        while inState.legalBalls < 300 and inState.wickets < 10:
+        while inState.legalBalls < 300 and inState.wickets < 10 and inState.total <= target:
             word = 'END'
             while word == 'END':
-                word = self.outcomeMarkov.generate_next_word(' '.join(prefix)) # do not allow match to end early
+                word = self.outcomeMarkov.generate_next_word(' '.join(prefix))  # do not allow match to end early
             prefix[0] = prefix[1]
             prefix[1] = word
-            thisModel = self.models[word+'.txt']
-            inState.updateState(outcomeDict[word])
-            print(word)                                                         # risk of infinite loop if destined for END
-            print(thisModel.generate_sequence(thisModel.startPrefix,30))
-            inState.printState()
-            # if inState.legalBalls % 6 == 0 or inState.wickets == 10:
-            #   inState.printState()
-            #print('\n')
+            thisModel = self.models[word + '.txt']
+            inState.updateState(self.outcomeDict[word])
+            if showOutput:
+                if commentary:
+                    print(word)  # risk of infinite loop if destined for END
+                    print(thisModel.generate_sequence(thisModel.startPrefix, 30)[13:])
+                if scoreEveryBall:
+                    inState.printState()
+                else:
+                    if inState.legalBalls % 6 == 0 or inState.wickets == 10:
+                        inState.printState()
+                        print()
         # print(self.outcomeMarkov.generate_sequence('START', 5)[6:])    #  for outcomes.txt n = 1
+        return inState
+
+    def compareInnings(self, first, second):
+        print()
+        print('First Innings: ')
+        first.printState()
+        print('Second Innings: ')
+        second.printState()
+        firstTotal = first.runs + first.extras
+        secondTotal = second.runs + second.extras
+        if firstTotal > secondTotal:
+            print('Team A wins by', firstTotal - secondTotal, 'runs!')
+        elif firstTotal == secondTotal:
+            print('It\'s a tie!')
+        else:
+            print('Team B wins by', 10-second.wickets, 'wickets!')
 
     def createDictOfModels(self):
         models = {}
@@ -68,8 +130,6 @@ class MatchSim:
         for filename in os.listdir(address):
             models[filename] = MarkovComments(2,self.fileDirectory + '/' + filename)
         return models
-
-
 
     def initialiseOutcomes(self):                                           #       rebowl
         outcomeDict = {}                                                    #   R  E  v  W
@@ -97,3 +157,53 @@ class MatchSim:
         return outcomeDict
 
 Ms = MatchSim('scrapedSequences')  # scrapedSequences # 9-matches-punctuation-in-word
+#Ms.simulateMatch()
+
+
+'''         # find an innings with at least 400 runs
+iterations = 0
+inState = inningsState()
+highest = 0
+while inState.total < 400:
+    inState = Ms.simulateOneInnings(showOutput=False)
+    iterations = iterations + 1
+    if inState.total > highest:
+        highest = inState.total
+    if iterations%512 == 0:
+        print(iterations, 'iterations. Highest: ', highest)
+
+print(iterations,'iterations')
+
+inState.printStatistics()
+inState.printState()
+'''
+
+'''inState = inningsState()     # test the maiden detection
+dot = outcome('no_run', 0, 0, 1, 0)
+notDot = outcome('not_dot', 2, 0, 1, 0)
+wide = outcome('wide',0,1,0,0)
+bye = outcome('bye',0,1,1,0)
+wicket = outcome('wicket',0,0,1,1)
+
+inState.updateState(notDot)
+inState.updateState(notDot)
+inState.updateState(notDot)
+inState.updateState(wicket)
+inState.updateState(notDot)
+inState.updateState(notDot)
+inState.updateState(wide)
+inState.updateState(dot)
+inState.updateState(dot)
+inState.updateState(dot)
+inState.updateState(dot)
+inState.updateState(dot)
+inState.updateState(dot)
+inState.updateState(dot)
+inState.updateState(dot)
+inState.updateState(wicket)
+inState.updateState(dot)
+inState.updateState(dot)
+inState.updateState(dot)
+inState.printState()
+inState.printStatistics()
+'''
